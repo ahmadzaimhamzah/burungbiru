68f84e7ccc1680290d8d2f5f8e28f65103a0da92
Ahmad Zaim Hamzah <ahmadzaimhamzah@gmail.com>
2017-03-19 20:42:35 +0000

    created pages controller. added index, home, profile and explore pages. changed root url to index page.

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+# The pages controller contains all of the code for any page inside of /pages
+class PagesController < ApplicationController
+  
+  # Backend code for pages/index
+  def index
+  end
+
+  # Backend code for pages/home
+  def home
+  end
+
+  # Backend code for pages/profile
+  def profile
+  end
+
+  # Backend code for pages/explore
+  def explore
+  end
+end
